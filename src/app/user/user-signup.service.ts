dc8800c72f3f5ff4255b7b07f2c8dcaa8d68b8e4
ganesh0479 <ganesh0479@gmail.com>
2020-09-19 00:29:20 +0530

    fix(service): updated backend service address for user component

--- src/app/user/user-signup.service.ts
+++ src/app/user/user-signup.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {User} from './model/user';
+import {GroupUtil} from '../util/group-util';
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +14,15 @@ export class UserSignupService {
 
   register(user: User): Observable<any> {
     console.log('User Service: ' + user);
-    return this.httpClient.post<any>('http://localhost:8080/api/v1/go-tiny/users', user);
+    return this.httpClient.post<any>(GroupUtil.apiUrl + '/api/v1/go-tiny/users', user);
   }
 
   login(user: User): Observable<any> {
     console.log('User Service: ' + user);
-    return this.httpClient.patch<any>('http://localhost:8080/api/v1/go-tiny/users', user);
+    return this.httpClient.patch<any>(GroupUtil.apiUrl + '/api/v1/go-tiny/users', user);
   }
 
   getAll(): Observable<any> {
-    return this.httpClient.get<any>('http://localhost:8080/api/v1/go-tiny/users');
+    return this.httpClient.get<any>(GroupUtil.apiUrl + '/api/v1/go-tiny/users');
   }
 }
